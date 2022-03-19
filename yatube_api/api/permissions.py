2c1bf139e53c6c64bd02aa2e948912d8526fe845
Александр Белей <beleysasha@gmail.com>
2022-03-19 20:04:47 +1200

    first commit

--- yatube_api/api/permissions.py
+++ yatube_api/api/permissions.py
@@ -5,8 +5,8 @@ class OwnerOrReadOnly(permissions.BasePermission):
 
     def has_permission(self, request, view):
         return (
-                request.method in permissions.SAFE_METHODS
-                or request.user.is_authenticated
+            request.method in permissions.SAFE_METHODS
+            or request.user.is_authenticated
             )
 
     def has_object_permission(self, request, view, obj):
@@ -16,4 +16,4 @@ class OwnerOrReadOnly(permissions.BasePermission):
 class ReadOnly(permissions.BasePermission):
 
     def has_permission(self, request, view):
-        return request.method in permissions.SAFE_METHODS 
+        return request.method in permissions.SAFE_METHODS
